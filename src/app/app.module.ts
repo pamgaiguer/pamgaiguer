c51070ed3a9d658551f8e690e38c4e879273f102
Pam Gaiguer <pamella.gaiguer@gmail.com>
2020-05-13 01:03:21 +1000

    updating angular version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './app.routing';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { MaterialComponentsModule } from './modules/material-components/material-components.module';
 import { FooterComponent } from './shared/footer/footer.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { HomeComponent } from './pages/home/home.component'
@@ -22,7 +21,6 @@ import { HomeComponent } from './pages/home/home.component'
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialComponentsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
